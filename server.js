9a1e23e7f623ce006ada77e5efb198109ce649c7
Fortunat Mutunda <3602825+FottyM@users.noreply.github.com>
2019-09-08 13:54:08 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const SitePdf = require('site-pdf');
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const n = require('nonce')();
+
+const readFile = util.promisify(fs.readFile);
+const app = express();
+const routes = express.Router();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+routes.post('/toPdf', (req, res) => {
+  const { href, hostname } = new URL(req.body.url);
+  const timeStamp = n();
+  const fileName = `${hostname}${timeStamp}.pdf`;
+  const filePath = path.resolve('pdfs', fileName);
+  const pdf = new SitePdf({
+    userAgent: 'sitePDF',
+    paperSize: {
+      format: 'A4',
+      orientation: 'portrait',
+      margin: {
+        left: '0.5cm', right: '0.5cm', top: '0.5cm', bottom: '0.5cm',
+      },
+    },
+  });
+
+  pdf
+    .create(href, `${filePath}`).then(() => {
+      readFile(`${filePath}`)
+        .then((pdfData) => {
+          res.status(201).json({ filePath, pdfData });
+        })
+        .catch((err) => res.status(400).json(err));
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+app.use('/api', routes);
+app.listen(PORT, () => console.log(`http://localhost:${PORT}/api`));
